f6f76771359abd4bfa68a7994bf9d25e5b69c305
Ifegwu <agbanyimdaniel29@gmail.com>
2022-09-06 14:43:39 +0000

    editing the webpage

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home - New changes has been made</h3>"
+    html = "<h3>Sklearn Prediction Home - tasting a new deploy</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
